ce74364e76e1273b04a862fede3d49ebf75efe81
daniel <damontanari@outlook.com>
2025-05-09 15:59:05 -0300

    att

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ def create_app():
     app = Flask(__name__, template_folder='templates')
 
     # Configurações do aplicativo usando variáveis de ambiente
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')  # Usando variável de ambiente
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')  # Usando variável de ambiente
     app.config['FLASK_ENV'] = os.getenv('FLASK_ENV')  # Usando variável de ambiente
